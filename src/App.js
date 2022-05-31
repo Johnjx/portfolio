c93a27cf7665dcba21da94b8738b3af8d002f929
John Ruddock <john.jxr@pm.me>
2022-05-31 19:55:37 -0400

    Added mock landing page with parallax scrolling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { AnimatePresence } from "framer-motion";
 import PreLoader from "./components/preLoader";
-import TestPage from "./components/testPage";
 
 function App() {
   const [isLoading, setIsLoading] = useState(true)
@@ -15,9 +14,55 @@ function App() {
   return (
     <>
     <AnimatePresence exitBeforeEnter>
-    {isLoading && <PreLoader/>}
+      {isLoading && <PreLoader/>}
     </AnimatePresence>
-    <TestPage/>
+    {!isLoading && <div style={{
+      height: '100vh',
+      overflowY: 'auto',
+      overflowX: 'hidden',
+      perspective: '10px'
+    }}>
+      <header
+      style={{
+        position: 'relative',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%',
+        transformStyle: 'preserve-3d',
+        zIndex: '-1'
+      }}>
+          <img
+          style={{
+            width: '100%',
+            height: '100%',
+            position: 'absolute',
+            objectFit: 'cover',
+            zIndex: '-1',
+            transform: 'translateZ(-10px) scale(2)'
+          }}
+          src="https://images.pexels.com/photos/572897/pexels-photo-572897.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+          alt="snow covered mountain in the evening"
+          />
+          <h1
+          style={{
+            zIndex: '-1',
+            fontSize: '6rem',
+            transform: 'translateZ(-5px) scale(1.5)'
+          }}
+          >Welcome!</h1>
+      </header>
+      <section
+      className="p-8 text-2xl bg-slate-700 text-white"
+      >
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </section>
+    </div>}
+    
     </>
   );
 }
--- src/components/testPage.js
+++
@@ -1,7 +0,0 @@
-export const TestPage = () => (
-    <div
-    className="bg-slate-400 h-screen w-screen z-0">Hello
-    </div>
-)
-
-export default TestPage
