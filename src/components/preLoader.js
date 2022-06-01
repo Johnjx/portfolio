bee5ff717d49e77c9be1f34bdb7b20cf666e8609
John Ruddock <john.jxr@pm.me>
2022-06-01 12:02:36 -0400

    Removed pre loader and parallax testing

--- src/components/preLoader.js
+++ src/components/preLoader.js
@@ -3,7 +3,7 @@ import AnimatedPage from "./animations/animatedPage"
 export const PreLoader = () => (
     <AnimatedPage>
     <div className="pre-loader">
-        <div className="pre-loader-gif"></div>
+    
     </div>
     </AnimatedPage>
 )
