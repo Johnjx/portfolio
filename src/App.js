422228030325c82b3984da925a89e0464194ccba
John Ruddock <john.jxr@pm.me>
2022-06-01 13:21:28 -0400

    Projects section frame in place

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Intro from "./components/intro";
+import Projects from "./components/projects";
 
 function App() {
 
   return (
     <>
     <Intro/>
+    <Projects/>
     </>
   );
 }
---
+++ src/components/projects.js
@@ -0,0 +1,24 @@
+export const Projects = () => (
+    <section className="projects">
+        <h2 className="text-center">Showcase</h2>
+        <div className="flex justify-center">
+            <button
+            className=""
+            >
+            Previous
+            </button>
+            <button
+            className=""
+            >
+            Next
+            </button>
+        </div>
+        <div className="h-2/4 bg-gray-100 m-4 flex justify-center items-center">
+            <article className="feature">
+
+            </article>
+        </div>
+    </section>
+)
+
+export default Projects
