e34d71eb7fc875cc17226e87efb1907081a3ccac
John Ruddock <john.jxr@pm.me>
2022-06-01 16:34:13 -0400

    Work on Intro section

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,28 +1,118 @@
 export const Intro = () => (
     <div>
-        <h1>Welcome to my digital home.</h1>
+        <h1
+        className="text-center text-5xl p-6"
+        >John Ruddock
+        </h1>
+        <h2
+        className="text-center text-4xl p-7"
+        >Full Stack Web Developer.
+        </h2>
+        <div className="h-3 w-1/3 mx-auto mb-4 bg-red-300">
+        </div>
         <div className="flex">
-            <section className="w-2/4">
-                <h2>Vision.</h2>
-                <p>
+            <section className="w-2/4 p-4">
+                <h2 className="text-3xl p-2">Vision.</h2>
+                <div className="h-1 w-1/3 bg-red-300">
+                </div>
+                <p className="p-2 leading-loose">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Condimentum mattis pellentesque id nibh. Amet facilisis magna etiam tempor orci eu lobortis elementum nibh. Enim nulla aliquet porttitor lacus luctus accumsan tortor posuere ac. Aenean pharetra magna ac placerat. Donec massa sapien faucibus et. Tristique nulla aliquet enim tortor at auctor. Urna nunc id cursus metus aliquam. Amet mauris commodo quis imperdiet massa tincidunt nunc pulvinar sapien. Bibendum enim facilisis gravida neque convallis a cras semper.      
                 </p>
             </section>
-            <section className="w-2/4">
-                <h2>Skillset.</h2>
-                <div className="bg-slate-400 h-3/4 flex flex-col justify-evenly">
+            <section className="w-2/4 p-4">
+                <h2 className="text-3xl p-2">Skillset.</h2>
+                <div className="h-1 w-1/3 bg-red-300">
+                </div>
+                <div className="h-3/4 flex flex-col justify-evenly p-2">
                     <section className="skill-row">
-                        <div className="skill-cell"></div>
-                        <div className="skill-cell"></div>
+                        <div className="skill-cell">
+                            <div className="skill-inner bg-yellow-200">
+                            Javascript
+                            </div>
+                            <div className="skill-inner bg-yellow-200">
+                            ★★★★★
+                            </div>
+                        </div>
+                        <div className="skill-cell">
+                            <div className="skill-inner bg-blue-300">
+                            CSS
+                            </div>
+                            <div className="skill-inner bg-blue-300">
+                            ★★★★☆
+                            </div>
+                        </div>
                     </section>
                     <section className="skill-row">
-
+                        <div className="skill-cell">
+                            <div className="skill-inner bg-cyan-200">
+                            React
+                            </div>
+                            <div className="skill-inner bg-cyan-200">
+                            ★★★★★
+                            </div>
+                        </div>
+                        <div className="skill-cell">
+                            <div className="skill-inner bg-red-300">
+                            React Router DOM
+                            </div>
+                            <div className="skill-inner bg-red-300">
+                            ★★★★☆
+                            </div>
+                        </div>
                     </section>
                     <section className="skill-row">
-
+                        <div className="skill-cell">
+                            <div className="skill-inner bg-orange-200">
+                            HTML
+                            </div>
+                            <div className="skill-inner bg-orange-200">
+                            ★★★★★
+                            </div>
+                        </div>
+                        <div className="skill-cell">
+                            <div className="skill-inner bg-teal-200">
+                            Tailwind CSS
+                            </div>
+                            <div className="skill-inner bg-teal-200">
+                            ★★★★☆
+                            </div>
+                        </div>
                     </section>
                     <section className="skill-row">
-
+                        <div className="skill-cell">
+                            <div className="skill-inner bg-purple-200">
+                            Axios
+                            </div>
+                            <div className="skill-inner bg-purple-200">
+                            ★★★★★
+                            </div>
+                        </div>
+                        <div className="skill-cell">
+                            <div className="skill-inner bg-gray-300">
+                            Cypress
+                            </div>
+                            <div className="skill-inner bg-gray-300">
+                            ★★★★☆
+                            </div>
+                        </div>
+                    </section>
+                    <section className="skill-row">
+                        <div className="skill-cell">
+                            <div className="skill-inner bg-purple-400">
+                            Redux
+                            </div>
+                            <div className="skill-inner bg-purple-400">
+                            ★★★★☆
+                            </div>
+                        </div>
+                        <div className="skill-cell">
+                            <div className="skill-inner bg-pink-200">
+                            Framer Motion
+                            </div>
+                            <div className="skill-inner bg-pink-200">
+                            ★★★★☆
+                            </div>
+                        </div>
                     </section>
                 </div>
             </section>
@@ -31,3 +121,4 @@ export const Intro = () => (
 )
 
 export default Intro
+
